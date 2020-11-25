49a281d5bf49249f779e9178c7a617ba365bd85e
IcedCrescent <35650172+IcedCrescent@users.noreply.github.com>
2020-11-26 00:26:55 +0700

    Part 5: AnimationPlayer

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,12 @@ const MAX_SPEED = 80
 
 var velocity = Vector2.ZERO
 
+# onready var animation_player = $AnimationPlayer
+var animation_player: AnimationPlayer = null
+
+func _ready():
+	animation_player = $AnimationPlayer
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -14,8 +20,14 @@ func _physics_process(delta):
 	
 	# multiply by delta to match real world time instead of each frame, in case the game lags
 	if input_vector != Vector2.ZERO:
+		# if input_vector.x > 0:
+		#	animation_player.play("RunRight")
+		#else:
+		#	animation_player.play("RunLeft")
+		animation_player.play("RunRight" if input_vector.x > 0 else "RunLeft")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATON * delta)
 	else:
+		animation_player.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
